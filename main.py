c0c63f6a9b2142d912f1edfdf8a64e06d95782dd
Jisoo Kim <ooSoo2Soo2oo@gmail.com>
2024-05-12 10:10:36 +0900

    main 파일에도 예외처리 추가

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 import uvicorn
 import app
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", port=9999, log_level="debug",
-                proxy_headers=True, reload=True)
+    try:
+        uvicorn.run("app.app:app", host="0.0.0.0", port=9999, log_level="debug",
+                    proxy_headers=True, reload=True)
+    except Exception as e:
+        print("에러 발생:", e)
